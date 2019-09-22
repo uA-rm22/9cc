7bb0494029af27cf1fb7ea9d88bb7142a5e4d3ab
uA-rm22 <uA.rm22@gmail.com>
2019-09-22 17:16:01 +0900

    加算と減算を追加

--- 9cc.c
+++ 9cc.c
@@ -7,9 +7,26 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+	char *p=argv[1];
+
   printf(".global main\n");
 	printf("main:\n");
-  printf("  mov r0, #%d\n", atoi(argv[1]));
+  printf("  mov r0, #%ld\n", strtol(p, &p, 10));
+
+	while(*p){
+		if(*p == '+'){
+			p++;
+			printf("  add r0, r0, #%ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		if(*p == '-'){
+			p++;
+			printf("  sub r0, r0, #%ld\n", strtol(p, &p, 10));
+			continue;
+		}
+	}
+
   printf("  bx lr\n");
   return 0;
 }
