4cec61decde5926c335a9dffc70b9e75d897010e
uA-rm22 <uA.rm22@gmail.com>
2019-09-22 23:23:02 +0900

    スタックによる四則演算用の関数を追加。ただし、まだそれらの関数を呼んでいない。

--- 9cc.c
+++ 9cc.c
@@ -125,3 +125,110 @@ int main(int argc, char **argv) {
   printf("  bx lr\n");
   return 0;
 }
+
+typedef enum{
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+}NodeKind;
+
+typedef struct Node Node;
+
+struct Node{
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+Node *mul();
+Node *primary();
+Node *expr();
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val){
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *mul(){
+	Node *node = primary();
+	for(;;){
+		if(consume('*')){
+			node = new_node(ND_MUL, node, primary());
+		}else if(consume('/')){
+			node = new_node(ND_DIV, node, primary());
+		}else{
+			return node;
+		}
+	}
+}
+
+Node *expr(){
+	Node *node = mul();
+	for(;;){
+		if(consume('+')){
+			node = new_node(ND_ADD, node, mul());
+		}else if(consume('-')){
+			node = new_node(ND_SUB, node, mul());
+		}else{
+			return node;
+		}
+	}
+}
+
+Node *primary(){
+	if(consume('(')){
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	return new_node_num(expect_number());
+}
+
+void gen(Node *node){
+	if(node->kind == ND_NUM){
+		printf("mov32 r0, #%d\n", node->val);
+		printf("str r0, [sp, #-4]!");
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("ldr r1, [sp], #4\n");
+	printf("ldr r0, [sp], #4\n");
+
+	switch(node->kind){
+		case ND_ADD:
+			printf("add r0, r0, r1\n");
+			break;
+		case ND_SUB:
+			printf("sub r0, r0, r1\n");
+			break;
+		case ND_MUL:
+			printf("mul r0, r0, r1\n");
+			break;
+		case ND_DIV:
+			printf("eor r4, r0, r1\n");
+			printf("ratio DivMod R2, R0, R1, R3\n");//R2<= ans R0/R1 R3<=temp R0<=res
+
+			printf("cmp r4, #0\n");
+			printf("rsblt r2, #0\n");
+			printf("mov r0, r2\n");
+			break;
+	}
+	printf("str r0, [sp,#-4]!\n");
+}
