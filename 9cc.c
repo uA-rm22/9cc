3228c7608a930bfbd3ce541e426db02b64d8fd64
uA-rm22 <uA.rm22@gmail.com>
2019-09-23 11:27:11 +0900

    トークナイザを>= <= < > != ==　に対応させた。gen関数はまだ対応させていない。

--- 9cc.c
+++ 9cc.c
@@ -17,6 +17,7 @@ struct Token{
 	Token *next;
 	int val;
 	char *str;
+	int len;
 };
 
 Token *token;
@@ -35,17 +36,17 @@ void error_at(char *loc ,char *fmt,...){
 	exit(1);
 }
 
-bool consume(char op){
-	if(token->kind != TK_RESERVED || token->str[0] != op ){
+bool consume(char *op){
+	if(token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len) ){
 		return false;
 	}
 	token = token->next;
 	return true;
 }	
 
-void expect(char op){
-	if(token->kind != TK_RESERVED || token->str[0] != op){
-		error_at(token->str, "'%c'ではありません",op);
+void expect(char *op){
+	if(token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len) ){
+		error_at(token->str, "'%s'ではありません",op);
 	}
 	token = token->next;
 }
@@ -63,10 +64,11 @@ bool at_eof(){
 	return token->kind==TK_EOF;
 }
 
-Token *new_token(TokenKind kind, Token *cur, char *str){
+Token *new_token(TokenKind kind, Token *cur, char *str, int len){
 	Token *tok = calloc(1, sizeof(Token));
 	tok->kind = kind;
 	tok->str=str;
+	tok->len = len;
 	cur->next = tok;
 	return tok;
 }
@@ -82,20 +84,28 @@ Token *tokenize(char *p){
 			continue;
 		}
 
-		if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'){
-			cur = new_token(TK_RESERVED, cur, p++);
+		if(*p == '<' || *p == '>' || *p == '=' || *p == '!' ){
+			if(*(p+1) == '='){
+				cur = new_token(TK_RESERVED, cur, p, 2);
+				p = p + 2;
+				continue;
+			}
+		}
+
+		if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'|| *p == '>' || *p == '<'){
+			cur = new_token(TK_RESERVED, cur, p++, 1);
 			continue;
 		}
 
 		if(isdigit(*p)){
-			cur=new_token(TK_NUM, cur, p);
+			cur=new_token(TK_NUM, cur, p, 1);
 			cur->val=strtol(p, &p, 10);
 			continue;
 		}
 
 		error_at(token->str, "トークナイズできません");
 	}
-	new_token(TK_EOF, cur, p);
+	new_token(TK_EOF, cur, p, 1);
 	return head.next;
 }
 
@@ -139,9 +149,9 @@ Node *new_node_num(int val){
 Node *mul(){
 	Node *node = unary();
 	for(;;){
-		if(consume('*')){
+		if(consume("*")){
 			node = new_node(ND_MUL, node, unary());
-		}else if(consume('/')){
+		}else if(consume("/")){
 			node = new_node(ND_DIV, node, unary());
 		}else{
 			return node;
@@ -152,9 +162,9 @@ Node *mul(){
 Node *expr(){
 	Node *node = mul();
 	for(;;){
-		if(consume('+')){
+		if(consume("+")){
 			node = new_node(ND_ADD, node, mul());
-		}else if(consume('-')){
+		}else if(consume("-")){
 			node = new_node(ND_SUB, node, mul());
 		}else{
 			return node;
@@ -163,9 +173,9 @@ Node *expr(){
 }
 
 Node *primary(){
-	if(consume('(')){
+	if(consume("(")){
 		Node *node = expr();
-		expect(')');
+		expect(")");
 		return node;
 	}
 
@@ -173,10 +183,10 @@ Node *primary(){
 }
 
 Node *unary(){
-	if(consume('+')){
+	if(consume("+")){
 		return primary();
 	}
-	if(consume('-')){
+	if(consume("-")){
 		return new_node(ND_SUB, new_node_num(0), primary());
 	}
 	return primary();
@@ -184,7 +194,6 @@ Node *unary(){
 
 void gen(Node *node){
 	if(node->kind == ND_NUM){
-		char *tmp;
 
 		printf("ldr r0,=%d\n", node->val);
 		printf("push {r0}\n");
